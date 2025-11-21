a9d937cbb53cb55a4e433b7c37fa7d370a6f4776
Gaelstevens <163824683+Gaelstevens@users.noreply.github.com>
2025-11-21 15:31:19 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
 import asyncpg
 import os
+import ssl
 
 async def get_db():
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE  # nécessaire pour Supabase sur Vercel
+
     return await asyncpg.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME'),
-        port=5432
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=5432,
+        ssl=ssl_context
     )
